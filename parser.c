4632d93152085b96fef71365e3ac6bba36d2cb7c
wycky1990 <wyclifffeomondi@gmail.com>
2024-08-18 21:45:04 -0700

    simple shell project

---
+++ parser.c
@@ -0,0 +1,86 @@
+#include "shell.h"
+
+/**
+ * is_cmd - fuction that determines if a file is executable
+ * @info: info struct
+ * @path: path to the file
+ * Return: 1 if true, 0 otherwise
+ */
+
+int is_cmd(info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * dup_chars - function that duplicates characters
+ * @pathstr: the PATH string
+ * @start: starting index
+ * @stop: stopping index
+ * Return: pointer to new buffer
+ */
+
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int a = 0, b = 0;
+
+	for (b = 0, a = start; a < stop; a++)
+		if (pathstr[a] != ':')
+			buf[b++] = pathstr[a];
+	buf[b] = 0;
+	return (buf);
+}
+
+/**
+ * find_path - function that finds cmd in the PATH string
+ * @info: the info struct
+ * @pathstr: the PATH string
+ * @cmd: the cmd to find
+ *
+ * Return: full path of cmd if found or NULL
+ */
+char *find_path(info_t *info, char *pathstr, char *cmd)
+{
+	int j = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[j] || pathstr[j] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, j);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathstr[j])
+				break;
+			curr_pos = j;
+		}
+		j++;
+	}
+	return (NULL);
+}
---
+++ realloc.c
@@ -0,0 +1,64 @@
+#include "shell.h"
+
+/**
+ **_memset - a function fills memory with a constant byte
+ *@s: pointer to a  memory
+ *@b: byte to fill *s
+ *@n: bytes to be filled
+ *Return: (s) a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int j;
+
+	for (j = 0; j < n; j++)
+		s[j] = b;
+	return (s);
+}
+
+/**
+ * ffree - function that frees a string
+ * @pp: string of strings
+ */
+
+void ffree(char **pp)
+{
+	char **b = pp;
+
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(b);
+}
+
+/**
+ * _realloc - fuction that reallocates a block of memory
+ * @ptr: pointer to previous malloc'ated block
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
+ * Return: pointer to the block.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *a;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	p = malloc(new_size);
+	if (!a)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		a[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (a);
+}
