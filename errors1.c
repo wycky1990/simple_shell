59bfc6b388515991d1bf433fb445dc6ce756c0c8
martinondiwa <surekondiwa@gmail.com>
2024-08-28 13:38:28 -0700

    betty

--- errors1.c
+++ errors1.c
@@ -9,24 +9,24 @@
  */
 int _erratoi(char *s)
 {
-    int a = 0;
-    unsigned long int result = 0;
+	int a = 0;
+	unsigned long int result = 0;
 
-    if (*s == '+')
-        s++;
-    for (a = 0; s[a] != '\0'; a++)
-    {
-        if (s[a] >= '0' && s[a] <= '9')
-        {
-            result *= 10;
-            result += (s[a] - '0');
-            if (result > INT_MAX)
-                return (-1);
-        }
-        else
-            return (-1);
-    }
-    return (result);
+	if (*s == '+')
+		s++;
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			result *= 10;
+			result += (s[a] - '0');
+			if (result > INT_MAX)
+				return (-1);
+		}
+		else
+			return (-1);
+	}
+	return (result);
 }
 
 /**
@@ -36,13 +36,13 @@ int _erratoi(char *s)
  */
 void print_error(info_t *info, char *estr)
 {
-    _eputs(info->fname);
-    _eputs(": ");
-    print_d(info->line_count, STDERR_FILENO);
-    _eputs(": ");
-    _eputs(info->argv[0]);
-    _eputs(": ");
-    _eputs(estr);
+	_eputs(info->fname);
+	_eputs(": ");
+	print_d(info->line_count, STDERR_FILENO);
+	_eputs(": ");
+	_eputs(info->argv[0]);
+	_eputs(": ");
+	_eputs(estr);
 }
 
 /**
@@ -54,34 +54,34 @@ void print_error(info_t *info, char *estr)
  */
 int print_d(int input, int fd)
 {
-    int (*__putchar)(char) = _putchar;
-    int a, count = 0;
-    unsigned int _abs_, current;
+	int (*__putchar)(char) = _putchar;
+	int a, count = 0;
+	unsigned int _abs_, current;
 
-    if (fd == STDERR_FILENO)
-        __putchar = _eputchar;
-    if (input < 0)
-    {
-        _abs_ = -input;
-        __putchar('-');
-        count++;
-    }
-    else
-        _abs_ = input;
-    current = _abs_;
-    for (a = 1000000000; a > 1; a /= 10)
-    {
-        if (_abs_ / a)
-        {
-            __putchar('0' + current / a);
-            count++;
-        }
-        current %= a;
-    }
-    __putchar('0' + current);
-    count++;
+	if (fd == STDERR_FILENO)
+		__putchar = _eputchar;
+	if (input < 0)
+	{
+		_abs_ = -input;
+		__putchar('-');
+		count++;
+	}
+	else
+		_abs_ = input;
+	current = _abs_;
+	for (a = 1000000000; a > 1; a /= 10)
+	{
+		if (_abs_ / a)
+		{
+			__putchar('0' + current / a);
+			count++;
+		}
+		current %= a;
+	}
+	__putchar('0' + current);
+	count++;
 
-    return (count);
+	return (count);
 }
 
 /**
@@ -94,29 +94,29 @@ int print_d(int input, int fd)
  */
 char *convert_number(long int num, int base, int flags)
 {
-    static char *array;
-    static char buffer[50];
-    char sign = 0;
-    char *ptr;
-    unsigned long n = num;
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
 
-    if (!(flags & CONVERT_UNSIGNED) && num < 0)
-    {
-        n = -num;
-        sign = '-';
-    }
-    array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
-    ptr = &buffer[49];
-    *ptr = '\0';
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
 
-    do {
-        *--ptr = array[n % base];
-        n /= base;
-    } while (n != 0);
+	do {
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
 
-    if (sign)
-        *--ptr = sign;
-    return (ptr);
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
 }
 
 /**
@@ -125,16 +125,16 @@ char *convert_number(long int num, int base, int flags)
  */
 void remove_comments(char *buf)
 {
-    int a;
+	int a;
 
-    for (a = 0; buf[a] != '\0'; a++)
-    {
-        if (buf[a] == '#' && (a == 0 || buf[a - 1] == ' '))
-        {
-            buf[a] = '\0';
-            break;
-        }
-    }
+	for (a = 0; buf[a] != '\0'; a++)
+	{
+		if (buf[a] == '#' && (a == 0 || buf[a - 1] == ' '))
+		{
+			buf[a] = '\0';
+			break;
+		}
+	}
 }
 
 /**
@@ -143,11 +143,11 @@ void remove_comments(char *buf)
  */
 void _eputs(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        _eputchar(str[i]);
-        i++;
-    }
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
 }
