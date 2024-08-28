3734c646959d7f8b764c01c372148a9c947ed81c
martinondiwa <surekondiwa@gmail.com>
2024-08-28 12:19:48 -0700

    gethistory

--- history.c
+++ history.c
@@ -1,175 +1,166 @@
 #include "shell.h"
 
 /**
- * input_buf - function that buffers chained commands
- * @info: parameter struct
- * @buf: address of buffer
- * @len: address of len var
+ * get_history_file - Gets the history file path.
+ * @info: The parameter struct.
  *
- * Return: bytes read
+ * Return: Allocated string containing the history file path.
  */
-
-ssize_t input_buf(info_t *info, char **buf, size_t *len)
+char *get_history_file(info_t *info)
 {
-	ssize_t r = 0;
-	size_t len_p = 0;
+    char *buf, *dir;
 
-	if (!*len) /* if nothing left in the buffer, fill it */
-	{
-		/*bfree((void **)info->cmd_buf);*/
-		free(*buf);
-		*buf = NULL;
-		signal(SIGINT, sigintHandler);
-#if USE_GETLINE
-		r = getline(buf, &len_p, stdin);
-#else
-		r = _getline(info, buf, &len_p);
-#endif
-		if (r > 0)
-		{
-			if ((*buf)[r - 1] == '\n')
-			{
-				(*buf)[r - 1] = '\0'; /* remove trailing newline */
-				r--;
-			}
-			info->linecount_flag = 1;
-			remove_comments(*buf);
-			build_history_list(info, *buf, info->histcount++);
-			/* if (_strchr(*buf, ';')) is this a command chain? */
-			{
-				*len = r;
-				info->cmd_buf = buf;
-			}
-		}
-	}
-	return (r);
+    dir = _getenv(info, "HOME=");
+    if (!dir)
+        return (NULL);
+
+    buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
+    if (!buf)
+        return (NULL);
+
+    buf[0] = 0;
+    _strcpy(buf, dir);
+    _strcat(buf, "/");
+    _strcat(buf, HIST_FILE);
+    return (buf);
 }
 
 /**
- * get_input - function that gets a line minus the newline
- * @info: parameter struct
+ * write_history - Creates a file or appends to an existing file.
+ * @info: The parameter struct.
  *
- * Return: bytes read
+ * Return: 1 on success, otherwise -1.
  */
-
-ssize_t get_input(info_t *info)
+int write_history(info_t *info)
 {
-	static char *buf; /* the ';' command chain buffer */
-	static size_t i, j, len;
-	ssize_t r = 0;
-	char **buf_p = &(info->arg), *p;
+    ssize_t fd;
+    char *filename = get_history_file(info);
+    list_t *node = NULL;
 
-	_putchar(BUF_FLUSH);
-	r = input_buf(info, &buf, &len);
-	if (r == -1) /* EOF */
-		return (-1);
-	if (len)	/* we have commands left in the chain buffer */
-	{
-		j = i; /* init new iterator to current buf position */
-		p = buf + i; /* get pointer for return */
+    if (!filename)
+        return (-1);
 
-		check_chain(info, buf, &j, i, len);
-		while (j < len) /* iterate to semicolon or end */
-		{
-			if (is_chain(info, buf, &j))
-				break;
-			j++;
-		}
+    fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
+    free(filename);
 
-		i = j + 1; /* increment past nulled ';'' */
-		if (i >= len) /* reached end of buffer? */
-		{
-			i = len = 0; /* reset position and length */
-			info->cmd_buf_type = CMD_NORM;
-		}
+    if (fd == -1)
+        return (-1);
 
-		*buf_p = p; /* pass back pointer to current command position */
-		return (_strlen(p)); /* return length of current command */
-	}
+    for (node = info->history; node; node = node->next)
+    {
+        _putsfd(node->str, fd);
+        _putfd('\n', fd);
+    }
 
-	*buf_p = buf; /* else not a chain, pass back buffer from _getline() */
-	return (r); /* return length of buffer from _getline() */
+    _putfd(BUF_FLUSH, fd);
+    close(fd);
+    return (1);
 }
 
 /**
- * read_buf - function that reads a buffer
- * @info: parameter struct
- * @buf: buffer
- * @i: size
+ * read_history - Reads history from a file.
+ * @info: The parameter struct.
  *
- * Return: a
+ * Return: Number of lines read on success, 0 otherwise.
  */
-
-ssize_t read_buf(info_t *info, char *buf, size_t *i)
+int read_history(info_t *info)
 {
-	ssize_t a = 0;
+    int i, last = 0, linecount = 0;
+    ssize_t fd, rdlen, fsize = 0;
+    struct stat st;
+    char *buf = NULL, *filename = get_history_file(info);
 
-	if (*i)
-		return (0);
-	a = read(info->readfd, buf, READ_BUF_SIZE);
-	if (a >= 0)
-		*i = a;
-	return (a);
+    if (!filename)
+        return (0);
+
+    fd = open(filename, O_RDONLY);
+    free(filename);
+
+    if (fd == -1)
+        return (0);
+
+    if (!fstat(fd, &st))
+        fsize = st.st_size;
+
+    if (fsize < 2)
+        return (0);
+
+    buf = malloc(sizeof(char) * (fsize + 1));
+    if (!buf)
+        return (0);
+
+    rdlen = read(fd, buf, fsize);
+    buf[fsize] = 0;
+
+    if (rdlen <= 0)
+        return (free(buf), 0);
+
+    close(fd);
+
+    for (i = 0; i < fsize; i++)
+    {
+        if (buf[i] == '\n')
+        {
+            buf[i] = 0;
+            build_history_list(info, buf + last, linecount++);
+            last = i + 1;
+        }
+    }
+
+    if (last != i)
+        build_history_list(info, buf + last, linecount++);
+
+    free(buf);
+    info->histcount = linecount;
+
+    while (info->histcount >= HIST_MAX)
+        delete_node_at_index(&(info->history), 0);
+
+    renumber_history(info);
+    return (info->histcount);
 }
 
 /**
- * _getline - function that gets the next line of input from STDIN
- * @info: parameter struct
- * @ptr: address of pointer to buffer, preallocated or NULL
- * @length: size of preallocated ptr buffer if not NULL
+ * build_history_list - Adds an entry to the history linked list.
+ * @info: Structure containing potential arguments. Used to maintain
+ *        the history linked list.
+ * @buf: Buffer containing the history entry.
+ * @linecount: The history line count.
  *
- * Return: s
+ * Return: Always 0.
  */
-
-int _getline(info_t *info, char **ptr, size_t *length)
+int build_history_list(info_t *info, char *buf, int linecount)
 {
-	static char buf[READ_BUF_SIZE];
-	static size_t i, len;
-	size_t a;
-	ssize_t r = 0, s = 0;
-	char *p = NULL, *new_p = NULL, *c;
+    list_t *node = NULL;
 
-	p = *ptr;
-	if (p && length)
-		s = *length;
-	if (i == len)
-		i = len = 0;
+    if (info->history)
+        node = info->history;
 
-	r = read_buf(info, buf, &len);
-	if (r == -1 || (r == 0 && len == 0))
-		return (-1);
+    add_node_end(&node, buf, linecount);
 
-	c = _strchr(buf + i, '\n');
-	a = c ? 1 + (unsigned int)(c - buf) : len;
-	new_p = _realloc(p, s, s ? s + a : a + 1);
-	if (!new_p) /* MALLOC FAILURE! */
-		return (p ? free(p), -1 : -1);
+    if (!info->history)
+        info->history = node;
 
-	if (s)
-		_strncat(new_p, buf + i, a - i);
-	else
-		_strncpy(new_p, buf + i, a - i + 1);
-
-	s += a - i;
-	i = a;
-	p = new_p;
-
-	if (length)
-		*length = s;
-	*ptr = p;
-	return (s);
+    return (0);
 }
 
 /**
- * sigintHandler -function that  blocks ctrl-C
- * @sig_num: the signal number
+ * renumber_history - Renumbers the history linked list after changes.
+ * @info: Structure containing potential arguments. Used to maintain
+ *        the history linked list.
  *
- * Return: void
+ * Return: The new history count.
  */
-
-void sigintHandler(__attribute__((unused))int sig_num)
+int renumber_history(info_t *info)
 {
-	_puts("\n");
-	_puts("$ ");
-	_putchar(BUF_FLUSH);
+    list_t *node = info->history;
+    int i = 0;
+
+    while (node)
+    {
+        node->num = i++;
+        node = node->next;
+    }
+
+    return (info->histcount = i);
 }
