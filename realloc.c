d51f2870a5ac0fc1b3d31de1bfbd06973b2fbc01
martinondiwa <surekondiwa@gmail.com>
2024-08-28 05:26:56 -0700

    realloc

--- realloc.c
+++ realloc.c
@@ -52,7 +52,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	p = malloc(new_size);
+	a = malloc(new_size);
 	if (!a)
 		return (NULL);
 
